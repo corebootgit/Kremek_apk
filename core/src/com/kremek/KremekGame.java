f927931e215b8c8c5626644bff34a363062d4c3b
corebootgit <coreboot@tlen.pl>
2019-10-07 23:16:17 +0200

    viewPort testing

--- core/src/com/kremek/KremekGame.java
+++ core/src/com/kremek/KremekGame.java
@@ -9,14 +9,19 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class KremekGame extends ApplicationAdapter {
+
+
 	SpriteBatch batch;
 	Texture img;
 
 	private TiledMap map;
 	private OrthoCachedTiledMapRenderer renderer;
 	private OrthographicCamera camera;
+	private FillViewport viewport;
 
 	int camera_xpos;
 
@@ -30,12 +35,24 @@ public class KremekGame extends ApplicationAdapter {
 
 		renderer = new OrthoCachedTiledMapRenderer(map);
 
-		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		int width = Gdx.graphics.getWidth(); // Can change to other
+		int height = Gdx.graphics.getHeight();
 
-		camera.viewportHeight = Gdx.graphics.getHeight();
-		camera.viewportWidth = Gdx.graphics.getWidth();
-		//camera.translate(camera.viewportWidth/2, camera.viewportHeight/2);
-		camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2,0);
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, width, height);
+		viewport = new FillViewport(width, height, camera);
+
+		//camera.position.set(camera.viewportWidth/2,camera.viewportHeight/2,0);
+		//viewport.apply(true);
+
+		//viewport.camera.position.set(768, 0, 0);
+
+//		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+//
+//		camera.viewportHeight = Gdx.graphics.getHeight();
+//		camera.viewportWidth = Gdx.graphics.getWidth();
+//		//camera.translate(camera.viewportWidth/2, camera.viewportHeight/2);
+//		camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2,0);
 
 	}
 
@@ -44,12 +61,25 @@ public class KremekGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		camera.position.set(camera_xpos, camera.viewportHeight/2,0);
-		if(camera_xpos > camera.viewportWidth) camera_xpos = 0;
-		camera_xpos += 8;
-		camera.update();
+//		camera.position.set(camera_xpos, camera.viewportHeight/2,0);
+//		if(camera_xpos > camera.viewportWidth) camera_xpos = 0;
+//		camera_xpos += 8;
+
+//		camera.update();
 		renderer.setView(camera);
 		renderer.render();
+
+		//Begin GameBatch
+		//this.batch.setProjectionMatrix(camera.combined);
+
+		//renderer.setView(viewport.camera);
+
+//		camera.position.set(camera_xpos, camera.viewportHeight/2,0);
+//		if(camera_xpos > camera.viewportWidth) camera_xpos = 0;
+//		camera_xpos += 8;
+//		camera.update();
+//		renderer.setView(camera);
+//		renderer.render();
 
 //		batch.begin();
 //		batch.draw(img, 0, 0, 1200, 1200);
